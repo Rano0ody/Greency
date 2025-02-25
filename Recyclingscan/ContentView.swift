36ad0e795e8b156d8bc9e2e3cdcfc68f4a4d6c05
Rano0ody <rand45042@gmail.com>
2025-02-25 16:19:35 +0300

    Initial Commit

---
+++ Recyclingscan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recyclingscan
+//
+//  Created by Rand abdullatif on 26/08/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Recyclingscan/RecyclingscanApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecyclingscanApp.swift
+//  Recyclingscan
+//
+//  Created by Rand abdullatif on 26/08/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct RecyclingscanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
