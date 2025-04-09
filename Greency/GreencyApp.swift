c995e96e4f074239c63e42cafa0c234875ddd468
Rano0ody <rand45042@gmail.com>
2025-04-09 15:57:32 +0300

    Initial Commit

---
+++ Greency/GreencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreencyApp.swift
+//  Greency
+//
+//  Created by Rand abdullatif on 11/10/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct GreencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
