7b06002c17f695ce9c79172fe5aa47eee06380e7
jood <joodhassan8899@icloud.com>
2025-04-14 15:23:23 +0300

    jjj

---
+++ Greency/.swift
@@ -0,0 +1,17 @@
+  .onAppear {
+        if let user = users.first {
+            userName = user.userName  // تأكد أن الخاصية userName موجودة هنا
+            email = user.email
+            password = user.password
+        }
+    }
+
+    // حفظ التغييرات في SwiftData
+    func saveChanges() {
+        if let user = users.first {
+            user.userName = userName
+            user.email = email
+            user.password = password
+            do {
+                try user.save()
+            } catch {
---
+++ Greency/UserData.swift
@@ -0,0 +1,8 @@
+//
+//  UserData.swift
+//  Greency
+//
+//  Created by joody on 15/10/1446 AH.
+//
+
+import Foundation
